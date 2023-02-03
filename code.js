b1a9545ffd1f8e4545426be8d4b6585b6933a922
victor-kslv <120047629+victor-kslv@users.noreply.github.com>
2023-02-02 22:31:02 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,6 @@
+onEvent("Victor-Kslv", "click", function( ) {
+	console.log("Victor-Kslv clicked!");
+});
+onEvent("Victor-Kslv", "keydown", function(event) {
+	console.log("Key pressed: " + event.key);
+});
